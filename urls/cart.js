22e70fc0dddeb15084da339748710581e0cce9b2
bhagyadas96 <bhagyadas96@gmail.com>
2019-03-06 22:43:14 +0530

    carturl

--- urls/cart.js
+++ urls/cart.js
@@ -4,8 +4,18 @@ const router = express.Router();
 const jwtUtil = require('../util/_jwt');
 const controller = require('../controllers/cart')
 
-router.post('/create', jwtUtil.verifyJWTTokenIsUser, async(req, res) => {
-    controller.getFromCart(req, res);
+router.post('/get', jwtUtil.verifyJWTTokenIsUser, async(req, res) => {
+    controller.getCart(req, res);
+});
+
+router.post('/add', jwtUtil.verifyJWTTokenIsUser, async(req, res) => {
+    controller.addToCart(req, res);
+});
+router.post('/update', jwtUtil.verifyJWTTokenIsUser, async(req, res) => {
+    controller.updateCart(req, res);
+});
+router.post('/delete', jwtUtil.verifyJWTTokenIsUser, async(req, res) => {
+    controller.deleteCart(req, res);
 });
 
 
